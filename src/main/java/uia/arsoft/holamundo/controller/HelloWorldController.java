2e09f1ff1cec5e23a63a8f394f907a6f7f8d352a
casandradha <a2219091@correo.uia.mx>
2023-10-01 18:18:35 -0600

    Update

--- src/main/java/uia/arsoft/holamundo/controller/HelloWorldController.java
+++ src/main/java/uia/arsoft/holamundo/controller/HelloWorldController.java
@@ -2,15 +2,36 @@ package uia.arsoft.holamundo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import uia.arsoft.holamundo.model.Contact;
+
+import javax.validation.Valid;
 
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello/{name}")
-    public String helloMethod(@PathVariable("name") String nombre, Model model){
-        model.addAttribute("name", nombre);
+    /*@GetMapping("/hello/{name}")
+    public String helloMethod(@PathVariable("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }*/
+
+    @GetMapping("/hello")
+    public String helloMethod(Model model){
+        model.addAttribute("contact", new Contact());
+        return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@Valid Contact contact, Errors errors, Model model){
+        if(errors.hasErrors()){
+            return "hello";
+        }
+        model.addAttribute("contact", new Contact());
+        model.addAttribute("contactInfo", contact);
         return "hello";
     }
 
